ae143d4a27c4af7a896dba3a2b158e6e18ee33dd
lynkliu <59218951+lynkliu@users.noreply.github.com>
2022-05-21 18:06:18 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -6,6 +6,7 @@ git clone  https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 # cd OpwnWRT代码目录
+cd OpwnWRT
 wget https://github.com/immortalwrt/immortalwrt/commit/c10101fc0cf186196a354a91a75bf2856630dd68.patch
 git apply c10101fc0cf186196a354a91a75bf2856630dd68.patch
 rm c10101fc0cf186196a354a91a75bf2856630dd68.patch
